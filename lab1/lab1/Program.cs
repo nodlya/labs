2a20183bc277a149f4ff73550f9af5f3f95ebd72
nodlya <nadya-popova-02@mail.ru>
2020-09-26 12:24:58 +0500

    lab2

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Доброго времени суток!");
+            Console.WriteLine("Ввведите своё имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите день вашего рождения");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввведите месяц вашего рождения");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввведите год вашего рождения");
+            int year = Convert.ToInt32(Console.ReadLine());
+            bool ok = true;
+
+            if (!((year <= 2020) & (year > 0) & (month > 0) & (month < 13) & (day > 0) & (day < 32)))
+                ok = false;
+            else
+                if ((month == 2) & (day > 28))
+                if (day > 29)
+                    ok = false;
+                else
+                  if (!(((year % 4 == 0) & (year % 100 != 0)) | (year % 400 == 0)))
+                    ok = false;
+
+            int age;
+
+            if (ok == false)
+                Console.WriteLine("Некорректная дата");
+            else
+                if (month < 9)
+            {
+                age = 2020 - year;
+                Console.WriteLine($"Привет, {name}. Ваш возраст равен {age} лет. Приятно познакомиться.");
+            }
+
+            else
+                   if (month >= 9)
+                if (month > 9)
+                {
+                    age = 2020 - year - 1;
+                    Console.WriteLine($"Привет, {name}. Ваш возраст равен {age} лет. Приятно познакомиться.");
+
+                }
+                else
+              if (day < 19)
+                {
+                    age = 2020 - year;
+                    Console.WriteLine($"Привет, {name}. Ваш возраст равен {age} лет. Приятно познакомиться.");
+                }
+                else
+                {
+                    age = 2020 - year - 1;
+                    Console.WriteLine($"Привет, {name}. Ваш возраст равен {age} лет. Приятно познакомиться.");
+                }
+
+        }
+    }
+}
---
+++ lab2/конь/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+namespace конь
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            int x1 = Convert.ToInt32(s1[0]);
+            int y1 = Convert.ToInt32(s1[1]);
+          
+            string s2 = Console.ReadLine();
+            int x2 = Convert.ToInt32(s2[0]);
+            int y2 = Convert.ToInt32(s2[1]);
+
+            Console.WriteLine("Если фигура - конь, введите 1, если ладья - 2, слон - 3, ферзь - 4");
+            int num =Convert.ToInt32(Console.ReadLine());
+           
+            if ((64 < x1) && (x1 < 73) && (64 < x2) && (x2 < 73) && (y1 > 48) && (y1 < 57) && (y2 > 48) && (y2 < 57))
+            {
+                switch (num)
+                {
+                    case 1:
+                        Knight(ref x1,x2,y1,y2);
+                        break;
+                    case 2:
+                        Rook(ref x1, x2, y1, y2);
+                        break;
+                    case 3:
+                        Bishop(ref x1, x2, y1, y2);
+                        break;
+                    case 4:
+                        Queen(ref x1, x2, y1, y2);
+                        break;
+                }
+            }
+            else
+                Console.WriteLine("категорически неверно");
+
+        }
+
+        static void Knight (ref int x1, int x2, int y1, int y2)
+        {
+            if ((Math.Abs(x1 - x2) + Math.Abs(y1 - y2)) == 3 && x1 != x2 && y1 != y2)
+                Console.WriteLine("верно");
+            else
+                Console.WriteLine("неверно");
+        }
+
+        static void Queen (ref int x1, int x2, int y1, int y2)
+        {
+            if ((x1==x2 && y1!=y2) || (x1!=x2 && y1==y2) || (x1+y1==x2+y2) || (Math.Abs(x1-x2+y1-y2) % 2==0))
+                Console.WriteLine("верно");
+            else
+                Console.WriteLine("неверно");
+        }
+
+        static void Rook(ref int x1, int x2, int y1, int y2)
+        {
+            if ((x1 + y1 == x2 + y2) || (Math.Abs(x1 - x2 + y1 - y2) % 2==0))
+                Console.WriteLine("верно");
+            else
+                Console.WriteLine("неверно");
+        }
+
+        static void Bishop(ref int x1, int x2, int y1, int y2)
+        {
+            if ((x1 == x2 && y1 != y2) || (x1 != x2 && y1 == y2))
+                Console.WriteLine("верно");
+            else
+                Console.WriteLine("неверно");
+        }
+    }
+}
