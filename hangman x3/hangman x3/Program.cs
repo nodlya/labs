8c79f92b3806fd95c1aaa60d1e1b67ce69b5d8f8
nodlya <nadya-popova-02@mail.ru>
2021-03-11 15:53:35 +0500

    буп

---
+++ hangman x3/hangman x3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace hangman_x3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Info.status==Status.Playing)
+            {
+                ScreenGame.DrawWord();
+                Info.lettersTried.Add(Convert.ToChar(Console.ReadLine()));
+                if (!Info.wordToGuess.Contains
+                    (Info.lettersTried[Info.lettersTried.Count - 1]))
+                    Info.triesLeft--;
+                if (Info.triesLeft == 0)
+                    Info.status = Status.Lose;
+                int tried = 0;
+                for (int i = 0; i < Info.wordToGuess.Length; i++)
+                    foreach (char b in Info.lettersTried)
+                        if (b == Info.wordToGuess[i])
+                            tried++;
+                if (tried == Info.wordToGuess.Length)
+                    Info.status = Status.Win;
+
+                ScreenGame.StatusUpdate();
+            }
+        }
+    }
+}
---
+++ hangman x3/hangman x3/stuff.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace hangman_x3
+{
+    public enum Status
+    {
+        Playing,
+        Win,
+        Lose
+    };
+    public static class Info
+    {
+        public static int triesLeft = 5;
+        public readonly static string wordToGuess = WordWork.GetRandomWord();
+        public static List<char> lettersTried = new List<char>();
+        public static Status status = Status.Playing; 
+    }
+
+    public static class WordWork
+    {
+        public static string GetRandomWord()
+        {
+            string[] readText = File.ReadAllLines("..\\..\\..\\original.txt");
+            Random rnd = new Random();
+            return readText[rnd.Next(readText.Length)];
+        }
+    }
+
+    public static class ScreenGame
+    {
+        static public void DrawWord()
+        {
+            Console.Clear();
+            for (int i=0; i<Info.wordToGuess.Length; i++)
+            {
+                bool flag = true;
+                foreach (char b in Info.lettersTried)
+                {
+                    if (b == Info.wordToGuess[i])
+                    {
+                        Console.Write(b);
+                        flag = false;
+                    }
+                }
+
+                if (flag)
+                    Console.Write("_");
+            }
+        }
+
+        static public void StatusUpdate()
+        {
+            if (Info.status == Status.Playing)
+                if (Info.wordToGuess.Contains
+                    (Info.lettersTried[Info.lettersTried.Capacity - 1]))
+                    Console.WriteLine("Да, эта буква есть в слове, осталось " +
+                        Info.triesLeft + " попыток");
+                else
+                    Console.WriteLine("Нет, этой буквы в слове нет, осталось " +
+                         Info.triesLeft + " попыток");
+            else if (Info.status == Status.Lose) Console.WriteLine("Вы проиграли, " +
+                "слово было " + Info.wordToGuess);
+            else Console.WriteLine("Вы победили!!!");
+        }
+    }
+}
